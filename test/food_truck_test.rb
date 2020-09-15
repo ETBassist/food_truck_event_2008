0ecccb6b6b7240168e0a4a4a671a03ccf9b8f9ff
Eugene Theriault <hybridbassist@gmail.com>
2020-09-15 12:16:04 -0700

    Create test for checking stock in food truck class

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -6,6 +6,7 @@ require './lib/food_truck'
 class FoodTruckTest < MiniTest::Test
   def setup 
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
   end
 
   def test_it_exists
@@ -16,5 +17,12 @@ class FoodTruckTest < MiniTest::Test
     assert_equal "Rocky Mountain Pies", @food_truck.name
     expected = {}
     assert_equal expected, @food_truck.inventory
+  end
+
+  def test_it_can_check_items_in_stock
+    assert_equal 0, @food_truck.check_stock(@item1)
+    @food_truck.stock(@item1, 30)
+    expected = {@item1 => 30}
+    assert_equal 30, @food_truck.check_stock(@item1)
   end
 end
