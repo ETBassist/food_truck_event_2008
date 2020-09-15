b8389d1a191ad6845910902dde48ea329499ad7d
Eugene Theriault <hybridbassist@gmail.com>
2020-09-15 12:32:20 -0700

    Add to setup method for later tests, create test for adding food trucks

--- test/event_test.rb
+++ test/event_test.rb
@@ -10,6 +10,15 @@ class EventTest < MiniTest::Test
     @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     @event = Event.new("South Pearl Street Farmers Market")
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @food_truck.stock(@item1, 35)
+    @food_truck.stock(@item2, 7)
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    @food_truck3.stock(@item1, 65)
   end
   
   def test_it_exists
@@ -19,5 +28,12 @@ class EventTest < MiniTest::Test
   def test_it_has_attributes
     assert_equal "South Pearl Street Farmers Market", @event.name
     assert_equal [], @event.food_trucks
+  end
+
+  def test_it_can_add_food_trucks
+    @event.add_food_truck(@food_truck)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    assert_equal [@food_truck, @food_truck2, @food_truck3], @event.food_truck
   end
 end
