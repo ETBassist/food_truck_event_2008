c87886561b67b7523eb04b467f5cd1a0054fdf7f
Eugene Theriault <hybridbassist@gmail.com>
2020-09-15 12:24:32 -0700

    Create test it exists for event class

---
+++ test/event_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/food_truck'
+
+class EventTest < MiniTest::Test
+  def setup
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @event = Event.new("South Pearl Street Farmers Market")
+  end
+  
+  def test_it_exists
+    assert_instance_of Event, @event
+  end
+end
