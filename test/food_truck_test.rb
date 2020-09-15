7bd2433261d97c659b20f4572bc61c26a8e3c007
Eugene Theriault <hybridbassist@gmail.com>
2020-09-15 12:12:15 -0700

    Create test for attributes in food truck

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -11,4 +11,10 @@ class FoodTruckTest < MiniTest::Test
   def test_it_exists
     assert_instance_of FoodTruck, @food_truck
   end
+
+  def test_it_can_get_name_and_attributes
+    assert_equal "Rocky Mountain Pies", @food_truck.name
+    expected = {}
+    assert_equal expected, @food_truck.inventory
+  end
 end
