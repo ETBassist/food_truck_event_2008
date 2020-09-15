c9a5d72598fd4362795d28811bd26a64c56c41aa
Eugene Theriault <hybridbassist@gmail.com>
2020-09-15 12:26:53 -0700

    Create test for attributes of event class

--- test/event_test.rb
+++ test/event_test.rb
@@ -15,4 +15,9 @@ class EventTest < MiniTest::Test
   def test_it_exists
     assert_instance_of Event, @event
   end
+
+  def test_it_has_attributes
+    assert_equal "South Pearl Street Farmers Market", @event.name
+    assert_equal [], @event.food_trucks
+  end
 end
